7dc5e64e7ba7f8a9a763109e56e4e7c3068319fc
HaoChen <lihaochen987@gmail.com>
2021-09-11 17:22:38 +1200

    Implemented add_review and get_reviews_for_book with appropriate unit tests.

--- library/book/services.py
+++ library/book/services.py
@@ -3,12 +3,14 @@ from typing import List, Iterable
 from library.adapters.repository import AbstractRepository
 from library.domain.model import leave_review, Review, Book
 
+
 class NonExistentBookException(Exception):
     pass
 
 
 class UnknownUserException(Exception):
     pass
+
 
 def add_review(book_id: int, review_rating: int, comment_text: str, user_name: str, repo: AbstractRepository):
     # Check that Book exists.
@@ -21,7 +23,35 @@ def add_review(book_id: int, review_rating: int, comment_text: str, user_name: s
         raise UnknownUserException
 
     # Create Review
-    review = leave_review(comment_text,review_rating, user, book)
+    review = leave_review(comment_text, review_rating, user, book)
 
     # Update the repository
     repo.add_review(review)
+
+
+def get_reviews_for_book(book_id: int, repo: AbstractRepository):
+    book = repo.get_book_by_id(book_id)
+
+    if book is None:
+        raise NonExistentBookException
+
+    return reviews_to_dict(book.reviews)
+
+
+# ============================================
+# Functions to convert model entities to dicts
+# ============================================
+
+def review_to_dict(review: Review):
+    review_dict = {
+        'user_name': review.user.user_name,
+        'review_rating': review.rating,
+        'book_id': review.book.book_id,
+        'review_text': review.review_text,
+        'timestamp': review.timestamp
+    }
+    return review_dict
+
+
+def reviews_to_dict(reviews: Iterable[Review]):
+    return [review_to_dict(review) for review in reviews]
---
+++ tests/unit/test_services.py
@@ -0,0 +1,22 @@
+from datetime import date
+
+import pytest
+
+from library.book import services as book_services
+
+def test_can_add_review(in_memory_repo):
+    book_id = 27036538
+    review_rating = 5
+    review_text = "Fantastic book, I was at the edge of my seat!"
+    user_name = "fmercury"
+
+    # Call the service layer to add the comment
+    book_services.add_review(book_id, review_rating, review_text, user_name, in_memory_repo)
+
+    # Retrieve the comments for the book from the repository
+    reviews_as_dict = book_services.get_reviews_for_book(book_id, in_memory_repo)
+
+    # Check that reviews include a review with the new review text
+    assert next(
+        (dictionary['review_text'] for dictionary in reviews_as_dict if dictionary['review_text'] == review_text),
+        None) is not None
