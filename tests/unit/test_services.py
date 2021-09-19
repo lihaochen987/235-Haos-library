eda8fe2ee52fc6b42d1311ad2c1611c62977fdc3
HaoChen <lihaochen987@gmail.com>
2021-09-19 19:31:02 +1200

    Imported authentication/services.py and associated tests from given COVID application.

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -3,6 +3,7 @@ from datetime import date
 import pytest
 
 from library.book import services as book_services
+from library.authentication import services as auth_services
 
 
 def test_can_add_review(in_memory_repo):
@@ -55,3 +56,45 @@ def test_can_get_book_by_ebook_status(in_memory_repo):
 def test_can_get_book_by_number_of_pages(in_memory_repo):
     assert str(book_services.get_book_by_number_of_pages(144, in_memory_repo)) == '[<Book War Stories, Volume 4, book id = 27036539>, <Book Sherlock Holmes: Year One, book id = 11827783>]'
     assert str(book_services.get_book_by_number_of_pages(27836478236, in_memory_repo)) == 'None'
+
+def test_can_add_user(in_memory_repo):
+    new_user_name = 'jz'
+    new_password = 'abcd1A23'
+
+    auth_services.add_user(new_user_name, new_password, in_memory_repo)
+
+    user_as_dict = auth_services.get_user(new_user_name, in_memory_repo)
+    assert user_as_dict['user_name'] == new_user_name
+
+    # Check that password has been encrypted.
+    assert user_as_dict['password'].startswith('pbkdf2:sha256:')
+
+
+def test_cannot_add_user_with_existing_name(in_memory_repo):
+    user_name = 'thorke'
+    password = 'abcd1A23'
+
+    with pytest.raises(auth_services.NameNotUniqueException):
+        auth_services.add_user(user_name, password, in_memory_repo)
+
+
+def test_authentication_with_valid_credentials(in_memory_repo):
+    new_user_name = 'pmccartney'
+    new_password = 'abcd1A23'
+
+    auth_services.add_user(new_user_name, new_password, in_memory_repo)
+
+    try:
+        auth_services.authenticate_user(new_user_name, new_password, in_memory_repo)
+    except AuthenticationException:
+        assert False
+
+
+def test_authentication_with_invalid_credentials(in_memory_repo):
+    new_user_name = 'pmccartney'
+    new_password = 'abcd1A23'
+
+    auth_services.add_user(new_user_name, new_password, in_memory_repo)
+
+    with pytest.raises(auth_services.AuthenticationException):
+        auth_services.authenticate_user(new_user_name, '0987654321', in_memory_repo)
