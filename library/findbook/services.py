c99b47f86f17f9b6ce963163c7067bd1702f8ddb
HaoChen <lihaochen987@gmail.com>
2021-09-20 13:23:28 +1200

    Created separate directory for finding books.

---
+++ library/findbook/services.py
@@ -0,0 +1,47 @@
+from typing import List, Iterable
+
+from library.adapters.repository import AbstractRepository
+from library.domain.model import leave_review, Review, Book
+
+
+class NonExistentBookException(Exception):
+    pass
+
+
+class UnknownUserException(Exception):
+    pass
+
+
+def get_book_by_id(book_id: int, repo: AbstractRepository):
+    book = repo.get_book_by_id(book_id)
+    return book
+
+
+def get_book_by_title(book_title: str, repo: AbstractRepository):
+    books = repo.get_book_by_title(book_title)
+    return books
+
+
+def get_book_by_author(author_name: str, repo: AbstractRepository):
+    books = repo.get_book_by_author(author_name)
+    return books
+
+
+def get_book_by_publisher(publisher_name: str, repo: AbstractRepository):
+    books = repo.get_book_by_publisher(publisher_name)
+    return books
+
+
+def get_book_by_release_year(year: int, repo: AbstractRepository):
+    books = repo.get_book_by_release_year(year)
+    return books
+
+
+def get_book_by_ebook_status(e_book_status: bool, repo: AbstractRepository):
+    books = repo.get_book_by_ebook_status(e_book_status)
+    return books
+
+
+def get_book_by_number_of_pages(pages: int, repo: AbstractRepository):
+    books = repo.get_book_by_number_of_pages(pages)
+    return books
--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -3,6 +3,7 @@ from datetime import date
 import pytest
 
 from library.book import services as book_services
+from library.findbook import services as find_book_services
 from library.authentication import services as auth_services
 
 
@@ -25,37 +26,37 @@ def test_can_add_review(in_memory_repo):
 
 
 def test_can_get_book_by_id(in_memory_repo):
-    assert str(book_services.get_book_by_id(834987324897934759, in_memory_repo)) == 'None'
-    assert str(book_services.get_book_by_id(11827783,
+    assert str(find_book_services.get_book_by_id(834987324897934759, in_memory_repo)) == 'None'
+    assert str(find_book_services.get_book_by_id(11827783,
                                             in_memory_repo)) == '<Book Sherlock Holmes: Year One, book id = 11827783>'
 
 
 def test_can_get_book_by_author(in_memory_repo):
-    assert str(book_services.get_book_by_title('HaoChen Li', in_memory_repo)) == 'None'
-    assert str(book_services.get_book_by_title('Sherlock Holmes: Year One',
+    assert str(find_book_services.get_book_by_title('HaoChen Li', in_memory_repo)) == 'None'
+    assert str(find_book_services.get_book_by_title('Sherlock Holmes: Year One',
                                                in_memory_repo)) == '[<Book Sherlock Holmes: Year One, book id = 11827783>]'
 
 
 def test_can_get_book_by_publisher(in_memory_repo):
-    assert str(book_services.get_book_by_publisher('HaoChen Li', in_memory_repo)) == 'None'
-    assert str(book_services.get_book_by_publisher('Dynamite Entertainment',
+    assert str(find_book_services.get_book_by_publisher('HaoChen Li', in_memory_repo)) == 'None'
+    assert str(find_book_services.get_book_by_publisher('Dynamite Entertainment',
                                                    in_memory_repo)) == '[<Book Sherlock Holmes: Year One, book id = 11827783>]'
 
 
 def test_can_get_book_by_release_year(in_memory_repo):
-    assert str(book_services.get_book_by_release_year(273648237, in_memory_repo)) == 'None'
-    assert str(book_services.get_book_by_release_year(2011,
+    assert str(find_book_services.get_book_by_release_year(273648237, in_memory_repo)) == 'None'
+    assert str(find_book_services.get_book_by_release_year(2011,
                                                       in_memory_repo)) == '[<Book Sherlock Holmes: Year One, book id = 11827783>, <Book 續．星守犬, book id = 18711343>]'
 
 
 def test_can_get_book_by_ebook_status(in_memory_repo):
-    assert str(book_services.get_book_by_ebook_status(True,
+    assert str(find_book_services.get_book_by_ebook_status(True,
                                                       in_memory_repo)) == '[<Book The Switchblade Mamma, book id = 25742454>, <Book Bounty Hunter 4/3: My Life in Combat from Marine Scout Sniper to MARSOC, book id = 35452242>, <Book She Wolf #1, book id = 30735315>, <Book D.Gray-man, Vol. 16: Blood & Chains, book id = 18955715>]'
 
 
 def test_can_get_book_by_number_of_pages(in_memory_repo):
-    assert str(book_services.get_book_by_number_of_pages(144, in_memory_repo)) == '[<Book War Stories, Volume 4, book id = 27036539>, <Book Sherlock Holmes: Year One, book id = 11827783>]'
-    assert str(book_services.get_book_by_number_of_pages(27836478236, in_memory_repo)) == 'None'
+    assert str(find_book_services.get_book_by_number_of_pages(144, in_memory_repo)) == '[<Book War Stories, Volume 4, book id = 27036539>, <Book Sherlock Holmes: Year One, book id = 11827783>]'
+    assert str(find_book_services.get_book_by_number_of_pages(27836478236, in_memory_repo)) == 'None'
 
 def test_can_add_user(in_memory_repo):
     new_user_name = 'jz'
