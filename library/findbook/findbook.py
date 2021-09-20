534b010ed34efa8b001083d91ab6ce7636d1a84c
HaoChen <lihaochen987@gmail.com>
2021-09-20 16:10:01 +1200

    Implemented find_book_by_release_year.

--- library/findbook/findbook.py
+++ library/findbook/findbook.py
@@ -12,84 +12,106 @@ findbook_blueprint = Blueprint(
 
 # Initialisation functions
 def initialise_forms():
-    return IdSearchForm(), AuthorSearchForm(), PublisherSearchForm()
+    return IdSearchForm(), AuthorSearchForm(), PublisherSearchForm(), ReleaseYearSearchForm()
 
-def render_page(id_form, author_form, publisher_form):
+def render_page(id_form, author_form, publisher_form, release_form):
     return render_template(
         'findbook/findbook.html',
         id_form=id_form,
         author_form=author_form,
         publisher_form = publisher_form,
+        release_form = release_form,
         find_book_url=url_for('findbook_bp.find_book'),
     )
 
 @findbook_blueprint.route('/find_book', methods=['GET', 'POST'])
 def find_book():
-    id_form, author_form, publisher_form = initialise_forms()
+    id_form, author_form, publisher_form, release_form = initialise_forms()
     # id_form = IdSearchForm()
     # author_form = AuthorSearchForm()
-    return render_page(id_form, author_form, publisher_form)
+    return render_page(id_form, author_form, publisher_form, release_form)
 
 
 @findbook_blueprint.route('/find_book_by_id', methods=['GET', 'POST'])
 def find_book_by_id():
-    id_form, author_form, publisher_form = initialise_forms()
+    id_form, author_form, publisher_form, release_form = initialise_forms()
 
     if id_form.validate_on_submit():
         if (services.get_book_by_id(id_form.book_id.data, repo.repo_instance) == None):
-            return render_page(id_form, author_form, publisher_form)
+            return render_page(id_form, author_form, publisher_form, release_form)
         else:
             return render_template(
                 'findbook/displaybooks.html',
                 books=services.get_book_by_id(id_form.book_id.data, repo.repo_instance),
                 id_form=id_form,
                 publisher_form=publisher_form,
+                release_form=release_form,
                 author_form=author_form
             )
     else:
-        return render_page(id_form, author_form, publisher_form)
+        return render_page(id_form, author_form, publisher_form, release_form)
 
 
 @findbook_blueprint.route('/find_book_by_author', methods=['GET', 'POST'])
 def find_book_by_author():
-    id_form, author_form, publisher_form = initialise_forms()
+    id_form, author_form, publisher_form, release_form = initialise_forms()
 
     if author_form.validate_on_submit():
         if (services.get_book_by_author(author_form.book_author.data, repo.repo_instance) == None):
-            return render_page(id_form, author_form, publisher_form)
+            return render_page(id_form, author_form, publisher_form, release_form)
         else:
             return render_template(
                 'findbook/displaybooks.html',
                 books=services.get_book_by_author(author_form.book_author.data, repo.repo_instance),
                 id_form=id_form,
                 publisher_form=publisher_form,
+                release_form=release_form,
                 author_form=author_form
             )
     else:
-        return render_page(id_form, author_form, publisher_form)
+        return render_page(id_form, author_form, publisher_form, release_form)
 
 @findbook_blueprint.route('/find_book_by_publisher', methods=['GET', 'POST'])
 def find_book_by_publisher():
-    id_form, author_form, publisher_form = initialise_forms()
+    id_form, author_form, publisher_form, release_form = initialise_forms()
 
     if publisher_form.validate_on_submit():
         if (services.get_book_by_publisher(publisher_form.book_publisher.data, repo.repo_instance) == None):
-            return render_page(id_form, author_form, publisher_form)
+            return render_page(id_form, author_form, publisher_form, release_form)
         else:
             return render_template(
                 'findbook/displaybooks.html',
                 books=services.get_book_by_publisher(publisher_form.book_publisher.data, repo.repo_instance),
                 id_form=id_form,
                 publisher_form=publisher_form,
+                release_form=release_form,
                 author_form=author_form
             )
     else:
-        return render_page(id_form, author_form, publisher_form)
+        return render_page(id_form, author_form, publisher_form, release_form)
+
+@findbook_blueprint.route('/find_book_by_release_year', methods=['GET', 'POST'])
+def find_book_by_release_year():
+    id_form, author_form, publisher_form, release_form = initialise_forms()
+
+    if release_form.validate_on_submit():
+        if (services.get_book_by_release_year(release_form.book_year.data, repo.repo_instance) == None):
+            return render_page(id_form, author_form, release_form, release_form)
+        else:
+            return render_template(
+                'findbook/displaybooks.html',
+                books=services.get_book_by_release_year(release_form.book_year.data, repo.repo_instance),
+                id_form=id_form,
+                publisher_form=publisher_form,
+                release_form = release_form,
+                author_form=author_form
+            )
+    else:
+        return render_page(id_form, author_form, publisher_form, release_form)
 
 class IdSearchForm(FlaskForm):
     book_id = IntegerField("Book id", [DataRequired()])
     submit = SubmitField("Find by Id")
-
 
 class AuthorSearchForm(FlaskForm):
     book_author = StringField("Book author", [DataRequired()])
@@ -98,3 +120,7 @@ class AuthorSearchForm(FlaskForm):
 class PublisherSearchForm(FlaskForm):
     book_publisher = StringField("Book publisher", [DataRequired()])
     submit = SubmitField("Find by Publisher")
+
+class ReleaseYearSearchForm(FlaskForm):
+    book_year = IntegerField("Book release year", [DataRequired()])
+    submit = SubmitField("Find by Release Year")
