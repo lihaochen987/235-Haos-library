91cb8a197350d79df6d35f07135cd2415d078724
HaoChen <lihaochen987@gmail.com>
2021-10-12 12:33:46 +1300

    Satisfied test_database_populate_select_all_users in test_populate_database.py.

--- tests_db/unit/test_populate_database.py
+++ tests_db/unit/test_populate_database.py
@@ -24,24 +24,24 @@ def test_database_populate_inspect_table_names(database_engine):
 #             all_tag_names.append(row['tag_name'])
 #
 #         assert all_tag_names == ['New Zealand', 'Health', 'World', 'Politics']
-#
-# def test_database_populate_select_all_users(database_engine):
-#
-#     # Get table information
-#     inspector = inspect(database_engine)
-#     name_of_users_table = inspector.get_table_names()[4]
-#
-#     with database_engine.connect() as connection:
-#         # query for records in table users
-#         select_statement = select([metadata.tables[name_of_users_table]])
-#         result = connection.execute(select_statement)
-#
-#         all_users = []
-#         for row in result:
-#             all_users.append(row['user_name'])
-#
-#         assert all_users == ['thorke', 'fmercury']
-#
+
+def test_database_populate_select_all_users(database_engine):
+
+    # Get table information
+    inspector = inspect(database_engine)
+    name_of_users_table = inspector.get_table_names()[5]
+
+    with database_engine.connect() as connection:
+        # query for records in table users
+        select_statement = select([metadata.tables[name_of_users_table]])
+        result = connection.execute(select_statement)
+
+        all_users = []
+        for row in result:
+            all_users.append(row['user_name'])
+
+        assert all_users == ['thorke', 'fmercury', 'mjackson']
+
 # def test_database_populate_select_all_comments(database_engine):
 #
 #     # Get table information
